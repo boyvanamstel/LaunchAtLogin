1fcb91d5904455e4d9a8a9af760ceb705b071175
Boy van Amstel <boy@boyvanamstel.nl>
2012-08-07 11:59:15 +0200

    Initial Commit

---
+++ LaunchAtLogin/DCLALAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DCLALAppDelegate.h
+//  LaunchAtLogin
+//
+//  Created by Boy van Amstel on 07-08-12.
+//  Copyright (c) 2012 Danger Cove. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DCLALAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ LaunchAtLogin/DCLALAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  DCLALAppDelegate.m
+//  LaunchAtLogin
+//
+//  Created by Boy van Amstel on 07-08-12.
+//  Copyright (c) 2012 Danger Cove. All rights reserved.
+//
+
+#import "DCLALAppDelegate.h"
+
+@implementation DCLALAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
